5ef062e41d12670e5a65417034a4d01a6331bad2
Callum-James Smith <techdragon@tuta.io>
2019-11-09 18:03:19 +0000

    started implementation of operator overloading functions

--- ex1/Date.cpp
+++ ex1/Date.cpp
@@ -1,9 +1,15 @@
+/**
+ * Author: Callum-James Smith
+ * Date Created: 04-11-2019
+ * Last Updated: 09-11-2019
+ */
 #include <iostream>
 #include "Date.h"
 
 using namespace std;
 
 int main() {
+    // Testing
 	Date(12, 6, 2100);
     system("pause");
 }
@@ -21,9 +27,6 @@ Date::Date(unsigned int d,unsigned int m,unsigned int y) {
     this->y = y;
 
     int maxDays; // Stores the maximum number of days in that month
-    if (Date::isLeapYear()) {
-        cout << "This is a leap year" << endl;
-    } else cout << "Not a leap year" << endl;
 
     switch(m) {
         case (1 || 3 || 5 || 7 || 8 || 10 || 12):
@@ -63,4 +66,72 @@ bool Date::isLeapYear() const {
             else return false;
         } else return true;
     } else return false;
+}
+
+// Implement operator functions
+Date& Date::operator++() {
+// Overloading of ++ operator, this function will return a new value.
+// This value being; the Date increased by 1.
+    int maxDays; // Stores the maximum number of days in that month
+
+    switch(m) {
+        case (1 || 3 || 5 || 7 || 8 || 10 || 12):
+            maxDays = 31;
+            break;
+        case 2:
+            if (Date::isLeapYear()) {
+                maxDays = 29;
+            } else {
+                maxDays = 28;
+            }
+            break;
+        default:
+            maxDays = 30;
+            break;
+    }
+
+    if (d == maxDays) {
+        d = 01;
+        if (m == 12) {
+            m = 01;
+            y += 1;
+        } else m += 1;
+    } else {
+        d += 1;
+    }
+
+    return *this;
+}
+
+Date& Date::operator--() {
+// Overloading of -- operator, this function will return a new value.
+// This value being; the Date decreased by 1.
+    int maxDays; // Stores the maximum number of days in that month
+
+    switch(m) {
+        case (1 || 3 || 5 || 7 || 8 || 10 || 12):
+            maxDays = 31;
+            break;
+        case 2:
+            if (Date::isLeapYear()) {
+                maxDays = 29;
+            } else {
+                maxDays = 28;
+            }
+            break;
+        default:
+            maxDays = 30;
+            break;
+    }
+
+    if (d == 1) {
+        d = maxDays;
+        if (m == 1) {
+            y -= 1;
+        } else m -= 1;
+    } else {
+        d -= 1;
+    }
+
+    return *this;
 }
