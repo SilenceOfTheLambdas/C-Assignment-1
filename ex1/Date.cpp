eebf86bcfe7d8600db2fb7abaf178bb3dc556a3b
Callum-James Smith <techdragon@tuta.io>
2019-11-09 17:03:14 +0000

    implemented isLeapYear() function

--- ex1/Date.cpp
+++ ex1/Date.cpp
@@ -3,6 +3,11 @@
 
 using namespace std;
 
+int main() {
+	Date(12, 6, 2100);
+    system("pause");
+}
+
 Date::Date() {
     this->d = 1;
     this->m = 1;
@@ -15,7 +20,10 @@ Date::Date(unsigned int d,unsigned int m,unsigned int y) {
     this->m = m;
     this->y = y;
 
-    int maxDays;
+    int maxDays; // Stores the maximum number of days in that month
+    if (Date::isLeapYear()) {
+        cout << "This is a leap year" << endl;
+    } else cout << "Not a leap year" << endl;
 
     switch(m) {
         case (1 || 3 || 5 || 7 || 8 || 10 || 12):
@@ -31,7 +39,6 @@ Date::Date(unsigned int d,unsigned int m,unsigned int y) {
         default:
             maxDays = 30;
             break;
-
     }
 
     // Error checking for input of date
@@ -42,6 +49,18 @@ Date::Date(unsigned int d,unsigned int m,unsigned int y) {
         cout << "Please enter a valid month!" << endl;
         d = 1; m = 1; y = 1999;
     if (y < 1000) 
-        cout << "Please use a valid year!" << endl;
+        cout << "Please enter a valid year!" << endl;
         d = 1; m = 1; y = 1999;
+}
+
+bool Date::isLeapYear() const {
+    // This will return true fi the year given is a leap year,
+    // False, if not a leap year
+    if (y % 4 == 0) {
+        if (y % 100 == 0) {
+            if (y % 400 == 0)
+                return true;
+            else return false;
+        } else return true;
+    } else return false;
 }
